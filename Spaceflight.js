2c345add363aa70130c041533aef2e65d1d76230
HyrulianHero <gmadkins.gm@gmail.com>
2015-12-25 08:34:32 -0600

    It rotates now, :D.

--- Spaceflight.js
+++ Spaceflight.js
@@ -1,1 +1,33 @@
-fill me up! 
+var keymap = 
+{
+    w:87, a:65, s:83, d:68
+}
+
+var ship = 
+{
+    velocity:0, direction:0
+}
+
+$(document).ready(function()
+{
+    $("body").on("keydown", function(event)
+	{
+	    console.log("TRIGGERED",event.which);
+	    //rotate the ship counter-clockwise
+	    if (event.which == keymap.a)
+	    {
+		ship.direction--;
+	    }
+	    
+	    //rotate the ship clockwise
+	    else if (event.which == keymap.d)
+	    {
+		ship.direction++;
+	    }
+
+	    $('.spaceship').transform("rotate",ship.direction + "deg");
+	    console.log(ship.direction);
+	});
+
+});
+
